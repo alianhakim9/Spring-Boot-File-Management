b9fa0fdcc7ed76dfcd7df9e36109008ce9584ad6
Alian Hakim <alianhakim9@gmail.com>
2022-09-05 06:54:53 +0700

    setup docker compose

---
+++ src/main/kotlin/id/alianhakim/api/SpringBootFileManagementApplication.kt
@@ -0,0 +1,11 @@
+package id.alianhakim.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootFileManagementApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootFileManagementApplication>(*args)
+}
---
+++ src/test/kotlin/id/alianhakim/api/SpringBootFileManagementApplicationTests.kt
@@ -0,0 +1,13 @@
+package id.alianhakim.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootFileManagementApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
